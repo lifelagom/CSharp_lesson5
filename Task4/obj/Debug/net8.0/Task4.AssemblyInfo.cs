a0d40f42684000eda700e47bd69eb257bac263a3
Vladimir <buyanov.vv@yandex.ru>
2023-12-04 00:32:16 +0700

    удалил старый файл gitignor

--- Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
+++ Task4/obj/Debug/net8.0/Task4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d560c5b27fe74bb46813ec07cb5fcfa32fd6dd0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
